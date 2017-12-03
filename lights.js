2d633a6790416e34d6ae9571993828dade3d6659
Paul Hayes <paul.hayes@entropedia.co.uk>
2017-12-03 20:42:16 +0000

    Clean up

--- lights.js
+++ lights.js
@@ -8,8 +8,8 @@ const url = require('url');
 const Plain = require('./effects/plain').Plain;
 const Fire = require('./effects/fire').Fire;
 
-const NUM_LEDS = parseInt(process.argv[2], 10) || 160;
-const STRIP_TYPE = process.argv[3] || "sk6812-grbw";
+const NUM_LEDS = 160;
+const STRIP_TYPE = "sk6812-grbw";
 
 const channels = ws281x.init({
   dma: 5,
@@ -54,55 +54,13 @@ setInterval(function() {
   if( currentEffect != null ){
     currentEffect.update(pixelData);
   }
-  /*
-  var middlePixel = NUM_LEDS/2;
-
-  //pixelData[middlePixel] = rgbw2Int(0,0,0, Math.floor(128*Math.random())+128 );
   
-  for (let i = 0; i < middlePixel; i++) {  
-    pixelData[i] = pixelData[i+1];
-    pixelData[NUM_LEDS-i-1] = pixelData[NUM_LEDS-i-2];       
-  }
-  if(Math.random()<0.1){
-    pixelData[middlePixel] = rgbw2Int(0,0,0, Math.floor(128*Math.random())+128 );    
-  }
-  else {
-   pixelData[middlePixel] = rgbw2Int(0,0,0, 128 );  
-  }
-
-
-  offset = (offset + 1) % 256;
-  //console.log(pixelData);
-  */
   ws281x.render();
 
   inc++;
 }, 1000 / 30);
 
 console.log('Press <ctrl>+C to exit.');
-
-// rainbow-colors, taken from http://goo.gl/Cs3H0v
-function colorwheel(pos) {
-  pos = 255 - pos;
-
-  if (pos < 85) {
-    return rgb2Int(255 - pos * 3, 0, pos * 3);
-  } else if (pos < 170) {
-    pos -= 85;
-    return rgb2Int(0, pos * 3, 255 - pos * 3);
-  } else {
-    pos -= 170;
-    return rgb2Int(pos * 3, 255 - pos * 3, 0);
-  }
-}
-
-function rgb2Int(r, g, b) {
-  return ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
-}
-
-function rgbw2Int(r, g, b, w) {
-  return ((w & 0xff) << 24) | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
-}
 
 
 function returnIndex(res){
@@ -150,11 +108,9 @@ const server = http.createServer((req, res) => {
     res.statusCode = 404;
     res.setHeader('Content-Type','text/plain');
     res.end("File Not Found");
-  }
-  //res.end('Hello World\n');
+  }  
 });
 
 server.listen(80, "0.0.0.0", () => {
   console.log("Server running");
 });
-
